0efd7ea462af3cbb3c01d8c504d5c9304076e621
pokie <pokie@Prasanth>
2025-07-21 22:46:57 +0530

    day_1

---
+++ C2TC_SIVARAM/src/DAY1/AddSub.java
@@ -0,0 +1,25 @@
+package DAY1;
+import java.util.*;
+public class AddSub {
+
+
+
+
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter a and b");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c= a+b;
+		System.out.println("Addition of a = "+a+" and b = "+b+" is : "+c);
+		c= a-b;
+		System.out.println("Subtraction of a = "+a+" and b = "+b+" is : "+c);
+		
+	
+
+}
+
+}
