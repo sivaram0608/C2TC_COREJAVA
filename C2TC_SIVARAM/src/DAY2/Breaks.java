23fb0620f0f71b62720cb409b3c50d6694524b27
pokie <pokie@Prasanth>
2025-07-21 22:58:52 +0530

    Day-2

---
+++ C2TC_SIVARAM/src/DAY2/Breaks.java
@@ -0,0 +1,15 @@
+package DAY2;
+
+public class Breaks {
+	public static void main(String[] args) {
+
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+
+	}
+
+
+}
