1eb36544c7a01574948ca5a32034e71223e3d8b4
pokie <pokie@Prasanth>
2025-07-21 22:47:11 +0530

    day_1

--- C2TC_SIVARAM/src/DAY1/DataType.java
+++ C2TC_SIVARAM/src/DAY1/DataType.java
@@ -1,5 +1,17 @@
 package DAY1;
 
-public class DataType {
+public class DataType {public static void main(String[]args) {
+	int num1 = 9 / 2; 
+	float num2 = 101 / 61;
+	double num3 = 10d / 6d;
+	float num4 = 5/2.5f; 
+	System.out.println("value 1=" + num1); 
+	System.out.println("value 2=" + num2);
+	System.out.println("value 3=" + num3);
+	System.out.println("value 4=" + num4);
+	
+	
+	
+}
 
 }
--- C2TC_SIVARAM/src/DAY1/Identifiers.java
+++ C2TC_SIVARAM/src/DAY1/Identifiers.java
@@ -1,5 +1,12 @@
 package DAY1;
 
 public class Identifiers {
+	public static void main(String[]args) {
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
 
+		String studentName="Aniket";
+		System.out.println("value is : "+ studentName);
+		
+	}
 }
