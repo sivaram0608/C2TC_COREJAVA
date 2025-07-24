0f4d314d46b8eaad1be2558163204aabc3991d48
Kalaimani <Kalaimani@DESKTOP-URBPEGL>
2025-07-24 14:33:22 +0530

    corejava

---
+++ C2TC_SIVARAM/src/DAY3/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,19 @@
+package DAY3.Encapsulation;
+
+public class EncapsulationDemo {
+
+	//Program to demonstrate class and object 
+		public static void main(String[] args) {
+			
+			//object creation 
+			OopsConceptDemo obj = new OopsConceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("Pooja");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
+
+	}
+
+
